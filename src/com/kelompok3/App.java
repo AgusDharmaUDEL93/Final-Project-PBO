57160e90c5baf4d13c7e70b84078427670b512c4
Agus Dharma <agusdharma@pop-os.localdomain>
2022-12-06 21:01:45 +0800

    adding sql driver

--- src/com/kelompok3/App.java
+++ src/com/kelompok3/App.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.kelompok3;
+import java.sql.*;
 
 /**
  *
@@ -11,6 +12,13 @@ package com.kelompok3;
 
 public class App {
     public static void main (String[] args){
-        System.out.println("Hello World");
+         try{
+            Class.forName("com.mysql.jdbc.Driver");  
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testjava", "admin", "admin");
+            Statement stmt =con.createStatement();  
+            System.out.println("Koneksi Berhasil");
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
